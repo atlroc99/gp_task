be619bf8964ae5464d99092e1ba2c1577a6e52ad
mzaman00 <mzaman@revegy.com>
2020-09-18 10:54:40 -0400

    rm annotation

--- src/main/java/com/gp/task/TaskApplication.java
+++ src/main/java/com/gp/task/TaskApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication()
-@EnableJpaRepositories(value = "com.gp.task.repository")
 public class TaskApplication {
 	public static void main(String[] args) {
 	    SpringApplication.run(TaskApplication.class, args);
