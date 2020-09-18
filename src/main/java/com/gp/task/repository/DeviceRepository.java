7e6b747fd25231d80374ef8fd62426e87a431166
mzaman00 <mzaman@revegy.com>
2020-09-18 00:08:39 -0400

    added @enabledJpaRepository

--- src/main/java/com/gp/task/repository/DeviceRepository.java
+++ src/main/java/com/gp/task/repository/DeviceRepository.java
@@ -2,9 +2,11 @@ package com.gp.task.repository;
 
 import com.gp.task.entity.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DeviceRepository extends JpaRepository<Device, String> {
     List<Device> findAll();
     Device findBySerialNoAndMachineCode(String serialNo, String machineCode);
